73c164a049930982a3f8cefba1a8ff2510afc022
radian462 <102207047+radian462@users.noreply.github.com>
2024-11-06 10:37:36 +0000

    blackでフォーマット

--- waybackmachine/main.py
+++ waybackmachine/main.py
@@ -11,7 +11,9 @@ from exceptions import *
 
 
 class waybackmachine:
-    def __init__(self, user_agent: str = "", proxies: dict = {}, debug: bool = False) -> None:
+    def __init__(
+        self, user_agent: str = "", proxies: dict = {}, debug: bool = False
+    ) -> None:
         self.user_agent = user_agent
         self.proxies = proxies
 
@@ -38,12 +40,17 @@ class waybackmachine:
         return r.url
 
     def get(self, url: str, timestamp: datetime | str = "latest") -> tuple:
-        if timestamp not in ["latest","oldest"] and not isinstance(timestamp, datetime):
-             raise ValueError("timestamp should be 'latest', 'oldest', or a datetime object")
-        
+        if timestamp not in ["latest", "oldest"] and not isinstance(
+            timestamp, datetime
+        ):
+            raise ValueError(
+                "timestamp should be 'latest', 'oldest', or a datetime object"
+            )
+
         timestamp = (
-            datetime.now() if timestamp == "latest" else 
-            datetime(2001, 10, 24, 0, 0, 0) if timestamp == "oldest" else timestamp
+            datetime.now()
+            if timestamp == "latest"
+            else datetime(2001, 10, 24, 0, 0, 0) if timestamp == "oldest" else timestamp
         )
         timestamp_str = timestamp.strftime("%Y%m%d%H%M%S")
 
@@ -52,7 +59,9 @@ class waybackmachine:
             "timestamp": timestamp_str,
         }
 
-        r = requests.get("https://archive.org/wayback/available", params=params, proxies=self.proxies)
+        r = requests.get(
+            "https://archive.org/wayback/available", params=params, proxies=self.proxies
+        )
 
         archive = r.json()["archived_snapshots"].get("closest")
         if archive:
@@ -61,11 +70,10 @@ class waybackmachine:
             return (archive_url, archive_timestamp)
         else:
             return ()
-        
-    def download(self,url:str,path:str|None=None) -> str:
+
+    def download(self, url: str, path: str | None = None) -> str:
         self.logger.debug(f"url:{url}")
         self.logger.debug(f"path:{path}")
-
 
         if not "web.archive.org/web/" in url:
             url = self.get(url)
@@ -74,47 +82,46 @@ class waybackmachine:
             url = url[0]
         else:
             raise NotFoundError("Archive Not Found")
-            
+
         playwright_proxy = {}
         if self.proxies:
             scheme = urlparse(url).scheme
             proxy = self.proxies.get(scheme)
             if proxy:
-                playwright_proxy = {'server': proxy}
-                if '@' in proxy:
-                    auth, ip = proxy.split('@')
-                    playwright_proxy['server'] = 'http://' + ip
-                    username, password = auth.split('//')[1].split(':')
-                    playwright_proxy['username'] = username
-                    playwright_proxy['password'] = password
+                playwright_proxy = {"server": proxy}
+                if "@" in proxy:
+                    auth, ip = proxy.split("@")
+                    playwright_proxy["server"] = "http://" + ip
+                    username, password = auth.split("//")[1].split(":")
+                    playwright_proxy["username"] = username
+                    playwright_proxy["password"] = password
 
         with sync_playwright() as playwright:
-            launch_options = {
-                'headless': True
-            }
+            launch_options = {"headless": True}
             if playwright_proxy:
-                launch_options['proxy'] = playwright_proxy
+                launch_options["proxy"] = playwright_proxy
 
             browser = playwright.chromium.launch(**launch_options)
             self.logger.debug(f"Browser launch")
-        
+
             page = browser.new_page()
             self.logger.debug(f"Access to {url}")
-            page.goto(url, wait_until='domcontentloaded')
+            page.goto(url, wait_until="domcontentloaded")
 
             if path is None:
-                timestamp = re.search(r'web\.archive\.org/web/(\d+)/', url).group(1)
+                timestamp = re.search(r"web\.archive\.org/web/(\d+)/", url).group(1)
                 path = f"{page.title()} - {timestamp}.mhtml"
 
             client = page.context.new_cdp_session(page)
-            mhtml = client.send("Page.captureSnapshot")['data']
-            with open(path, mode='w', encoding='UTF-8', newline='\n') as file:
+            mhtml = client.send("Page.captureSnapshot")["data"]
+            with open(path, mode="w", encoding="UTF-8", newline="\n") as file:
                 file.write(mhtml)
-            
+
             browser.close()
             self.logger.debug(f"Browser close")
 
         return path
+
 
 if __name__ == "__main__":
     wayback = waybackmachine(debug=True)
