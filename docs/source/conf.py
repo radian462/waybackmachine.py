1a4b722b5bfee1dfddffd6292b61237141ed1c08
radian462 <102207047+radian462@users.noreply.github.com>
2024-11-08 01:29:53 +0000

    テーマ変更

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,11 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+from pathlib import Path
+
+sys.path.insert(0, Path().resolve().as_posix())
+sys.path.insert(0, "../..")
 
 project = 'waybackmachine.py'
 copyright = '2024, radian462'
@@ -14,7 +19,11 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +33,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
